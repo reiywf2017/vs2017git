c6d1c7c51698ab4aa721e744d09bf3f635c90478
rei <1296773200@qq.com>
2020-01-27 15:54:34 +0800

    Configure2

--- vs2017git/web/Startup.cs
+++ vs2017git/web/Startup.cs
@@ -23,7 +23,7 @@ namespace web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            //Configure
+            //Configure2
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
